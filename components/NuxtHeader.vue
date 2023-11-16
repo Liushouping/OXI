f954563d458ae3d947d234b49e5358a7424b673d
liushouping <kose146352@gmail.com>
2023-11-16 17:26:29 +0800

    test

--- components/NuxtHeader.vue
+++ components/NuxtHeader.vue
@@ -5,6 +5,8 @@ const headerTextColor = ref("white");
 const headerShadow = ref(false);
 const headerIcon = ref(true);
 
+// const menu = ref(false);
+
 const handleScroll = () => {
 	const scrollY = window.scrollY || window.pageYOffset;
     headerHeight.value = scrollY > 110 ? 110 : 30;
@@ -33,7 +35,14 @@ onBeforeUnmount(() => {
 					<NuxtLink to="/">
 						<img src="/images/OXI-LOGO-Y.png" class="w-[60px]">
 					</NuxtLink>
-					<ul class="flex space-x-4 sm:space-x-6 font-bold text-[14px]">
+<!-- 					<div 
+					class="cursor-pointer"
+					@click="menu =! menu">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+						  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+						</svg>
+					</div> -->
+					<ul class="flex space-x-2 sm:space-x-6 font-bold text-[14px]">
 						<li><NuxtLink to="#MENU">MENU</NuxtLink></li>
 						<li><NuxtLink to="#GALLERY">GALLERY</NuxtLink></li>
 						<li><NuxtLink to="#ABOUT">ABOUT</NuxtLink></li>
@@ -49,5 +58,41 @@ onBeforeUnmount(() => {
 				</div>
 			</nav>
 		</header>
+<!-- 		<Teleport to="body">
+		<Transition name="slide-up">
+		    <div 
+		    v-if="menu" 
+		    class="fixed top-0 left-0 w-full h-screen bg-gray-900 flex justify-center items-center z-[101]">
+		      <button @click="menu =! menu" class="fixed top-8 right-4">
+		        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
+				  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+				</svg>
+		      </button>
+		      <div class="flex justify-center items-center">
+		      	<ul class="flex flex-col justify-center items-center text-white md:hidden space-y-6 font-bold text-[14px]">
+					<li><NuxtLink to="#MENU">MENU</NuxtLink></li>
+					<li><NuxtLink to="#GALLERY">GALLERY</NuxtLink></li>
+					<li><NuxtLink to="#ABOUT">ABOUT</NuxtLink></li>
+					<li><NuxtLink to="#CONTACT">CONTACT</NuxtLink></li>
+				</ul>
+		      </div>
+		    </div>
+		</Transition>
+		</Teleport> -->
 	</div>
 </template>
+
+<style>
+.slide-up-enter-active,
+.slide-up-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-up-enter-from {
+  opacity: 0;
+}
+
+.slide-up-leave-to {
+  opacity: 0;
+}
+</style>
