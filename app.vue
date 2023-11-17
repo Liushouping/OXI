616e6d07d836a6d28a2d2744a10547a16b4318db
liushouping <kose146352@gmail.com>
2023-11-17 20:19:16 +0800

    Test

--- app.vue
+++ app.vue
@@ -24,9 +24,13 @@ useHead({
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@300;500;900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Anton&family=Noto+Serif+TC:wght@300;500;900&display=swap');
 
 :root {
   font-family: 'Noto Serif TC', serif;
+}
+
+.font-anton {
+  font-family: 'Anton', sans-serif;
 }
 </style>
