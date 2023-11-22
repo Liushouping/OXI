9d5b6f70e467a76e88a2aeb69a727e2f09d61977
liushouping <kose146352@gmail.com>
2023-11-22 11:00:58 +0800

    test

--- components/NuxtBanner.vue
+++ components/NuxtBanner.vue
@@ -21,12 +21,15 @@
 			    data-aos-easing="ease-in-sine">LOOK MENU</NuxtLink>
 			</div>
 		</div>
-		<span data-aos="zoom-in" data-aos-duration="1000" class="w-[10px] h-[10px] absolute bottom-28 text-white animate-bounce z-20">
+		<span data-aos="zoom-in" data-aos-duration="1000" class="w-[10px] h-[10px] absolute bottom-28 text-white z-20">
 			<NuxtLink 
 			to="#ABOUT">
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+<!-- 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 				  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-				</svg>
+				</svg> -->
+				<div class="border-[2px] h-[24px] px-[12px] py-[21px] rounded-2xl flex justify-center items-start">
+				    <span class="hrheight"></span>
+				</div>
 			</NuxtLink>
 		</span>
 		<div class="bg-gray-900/40 w-full h-screen absolute bottom-0 left-0 z-[9]">
@@ -36,3 +39,24 @@
 		</div>
 	</div>
 </template>
+
+<style>
+	.hrheight {
+        position: absolute;
+        width: 2px;
+        height: 0px;
+        background: white;
+        animation-name: gg;
+        animation-iteration-count: infinite;
+        animation-duration: 2s;
+    }
+
+    @keyframes gg {
+        0% {
+            height: 0px;
+        }
+        100% {
+            height: 20px; 
+        }
+    }
+</style>
