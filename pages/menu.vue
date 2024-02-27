5bc7a47c349844fb431756314a60eb473c397c18
liushouping <kose146352@gmail.com>
2024-02-27 15:47:01 +0800

    Test

--- pages/menu.vue
+++ pages/menu.vue
@@ -36,14 +36,14 @@ onMounted(() => {
 				</div>
 				<div 
 				class="relative flex flex-col lg:flex-row space-y-4 lg:space-x-4 lg:space-y-0">
-					<a 
+<!-- 					<a 
 				    v-for="(image, index) in images"
 	      			:key="index"
 				    data-fancybox="gallery" 
 				    :data-src="image.src"
 				    class="w-full lg:w-1/2 cursor-zoom-in">
 				        <img loading="lazy" :src="image.src" :alt="image.alt" class="w-full">
-				    </a>
+				    </a> -->
 				</div>
 			</div>
 			<span class="absolute top-96 -left-12 text-[96px] text-gray-900 opacity-10 font-bold z-0">DRINK</span>
